5bd3eec1b9281887a2da2df65cac2607926e2a6e
Jonathan <jonathan@dataminded.net>
2022-10-15 11:26:30 -0400

    added function for game results

--- rps.js
+++ rps.js
@@ -1,12 +1,48 @@
-function getComputerChoice{
-   randomNumber = Math.floor(Math.random()*3)
-   if (randomNumber == 0){
+function getComputerChoice(){
+   let randomNumber = Math.floor(Math.random()*3)
+   if (randomNumber === 0){
     return "Rock"
    }
-   else if (randomNumber == 1){
+   else if (randomNumber === 1){
     return "Paper"
    }
-   else if (randomNumber == 2){
+   else if (randomNumber === 2){
     return "Scissors"
    }
 }
+
+let userSelection = prompt("Select Rock, Paper, or Scissors")
+
+function playRound(playerSelection,computerSelection){
+    if (playerSelection === "Rock" && computerSelection === "Rock"){
+        return "You both select Rock. It's a draw!"
+    }
+    else if (playerSelection == "Rock" && computerSelection == "Paper"){
+        return "Your opponent chooses paper. You lose!"
+    }
+    else if (playerSelection == "Rock"&& computerSelection == "Scissors") {
+        return "Your opponent chooses Scissors. You win!!"
+    }
+    else if (playerSelection == "Paper"&& computerSelection == "Rock") {
+        return "Your opponent chooses Rock. You win!!"
+    }
+    else if (playerSelection == "Paper"&& computerSelection == "Paper") {
+        return "Your opponent chooses Paper. It's a draw!"
+    }
+    else if (playerSelection == "Paper"&& computerSelection == "Scissors") {
+        return "Your opponent chooses Scissors. You lose!"
+    }
+    else if (playerSelection == "Scissors"&& computerSelection == "Rock") {
+        return "Your opponent chooses Rock. You lose!"
+    }
+    else if (playerSelection == "Scissors"&& computerSelection == "Paper") {
+        return "Your opponent chooses Paper. You win!!"
+    }
+    else if (playerSelection == "Scissors"&& computerSelection == "Scissors") {
+        return "Your opponent chooses Scissors. It's a draw!"
+    }
+}
+let computerSelection = getComputerChoice()
+console.log(computerSelection)
+console.log(userSelection)
+console.log(playRound(userSelection,computerSelection))
