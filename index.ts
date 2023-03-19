b6b0c6cbfc19a61290febfb817c3df81d42cf437
Jonathan <jonathan@dataminded.net>
2023-03-18 21:08:22 -0400

    added logic to the event listener to that it will declare the result of the match

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-function cpuChoice(){
+function randomChoice(){
     let randomNumber = Math.floor(Math.random()*3+1)
     switch(randomNumber){
         case 1:
@@ -16,16 +16,43 @@ function cpuChoice(){
 
 }
 
-console.log(cpuChoice())
-
 document.querySelector(".rock-img")?.addEventListener("click", function(){
-    console.log("rock-img working")
+    let computerChoice = randomChoice()
+    switch(computerChoice){
+        case "rock":
+            alert("tie")
+            break;
+        case "paper":
+            alert("loss")
+        case "scissors":
+            alert("win")
+    }
 })
 
 document.querySelector(".paper-img")?.addEventListener("click",function(){
     console.log("paper-img working")
+    let computerChoice = randomChoice()
+    switch(computerChoice){
+        case "rock":
+            alert("win")
+            break;
+        case "paper":
+            alert("tie")
+        case "scissors":
+            alert("lose")
+    }
 })
 
 document.querySelector(".scissors-img")?.addEventListener("click",function(){
     console.log("scissors-img working")
+    let computerChoice = randomChoice()
+    switch(computerChoice){
+        case "rock":
+            alert("lose")
+            break;
+        case "paper":
+            alert("win")
+        case "scissors":
+            alert("tie")
+    }
 })
